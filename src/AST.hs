7e8011b15b7d1a83f69b75063e51dcf48601346e
Maarten Vandercammen <mvdcamme@vub.ac.be>
2016-07-16 23:39:26 +0200

    Fix indentation

--- src/AST.hs
+++ src/AST.hs
@@ -1,61 +1,61 @@
 module AST where
 
-        import Type
+    import Type
 
-        type Name =                     String
+    type Name =                     String
 
-        data Declaration =              VarDeclaration Type Name
-                                        | FunDeclaration Type Name Declarations Body
-                                        deriving (Show, Eq)
+    data Declaration =              VarDeclaration Type Name
+                                    | FunDeclaration Type Name Declarations Body
+                                    deriving (Show, Eq)
 
-        type Declarations =             [Declaration]
+    type Declarations =             [Declaration]
 
-        data Statement =                AssignStmt Expression Expression
-                                        | BlockStmt Body
-                                        | ExpStmt Expression 
-                                        | ReturnStmt Expression
-                                        | ReadStmt Expression
-                                        | WriteStmt Expression
-                                        | WhileStmt Marker Expression Marker Body Marker
-                                        deriving (Show, Eq)
+    data Statement =                AssignStmt Expression Expression
+                                    | BlockStmt Body
+                                    | ExpStmt Expression 
+                                    | ReturnStmt Expression
+                                    | ReadStmt Expression
+                                    | WriteStmt Expression
+                                    | WhileStmt Marker Expression Marker Body Marker
+                                    deriving (Show, Eq)
 
-        type Statements =               [Statement]
+    type Statements =               [Statement]
 
-        data Body =                     Body Declarations Statements
-                                        deriving (Show, Eq)
+    data Body =                     Body Declarations Statements
+                                    deriving (Show, Eq)
 
-        data Expression =               VariableRefExp Name
-                                        | ArrayRefExp Expression Expression
-                                        | BinaryExp BinOperator Expression Expression
-                                        | UnaryExp UnOperator Expression
-                                        | FunctionAppExp Name Expressions
-                                        | LengthExp Expression
-                                        | NumberExp Integer
-                                        deriving (Show, Eq)
+    data Expression =               VariableRefExp Name
+                                    | ArrayRefExp Expression Expression
+                                    | BinaryExp BinOperator Expression Expression
+                                    | UnaryExp UnOperator Expression
+                                    | FunctionAppExp Name Expressions
+                                    | LengthExp Expression
+                                    | NumberExp Integer
+                                    deriving (Show, Eq)
 
-        type Expressions =              [Expression]
+    type Expressions =              [Expression]
 
-        data BinOperator =              PlusOp
-                                        | MinusOp
-                                        | TimesOp
-                                        | DivideOp
-                                        | EqualOp
-                                        | NequalOp
-                                        | GreaterOp
-                                        | GreaterEqualOp
-                                        | LessOp
-                                        | LessEqualOp
-                                        deriving (Show, Eq)
+    data BinOperator =              PlusOp
+                                    | MinusOp
+                                    | TimesOp
+                                    | DivideOp
+                                    | EqualOp
+                                    | NequalOp
+                                    | GreaterOp
+                                    | GreaterEqualOp
+                                    | LessOp
+                                    | LessEqualOp
+                                    deriving (Show, Eq)
 
-        data UnOperator =               MinusUnOp
-                                        | NotOp
-                                        deriving (Show, Eq)
+    data UnOperator =               MinusUnOp
+                                    | NotOp
+                                    deriving (Show, Eq)
 
-        type Location =                 Integer
+    type Location =                 Integer
 
-        data Marker =                   Marker Location
-                                        deriving (Show, Eq)
+    data Marker =                   Marker Location
+                                    deriving (Show, Eq)
 
-        data Goto =                     Goto Marker
-                                        deriving (Show, Eq)
+    data Goto =                     Goto Marker
+                                    deriving (Show, Eq)
 
